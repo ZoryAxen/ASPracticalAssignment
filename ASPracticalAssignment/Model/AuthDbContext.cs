740ff210627b369d1e75690e94c6d7174e3c56ab
Yi Shen <yishen890@gmail.com>
2023-01-27 16:19:00 +0800

    Add project files.

---
+++ ASPracticalAssignment/Model/AuthDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.IdentityEntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPracticalAssignment.Model
+{
+    public class AuthDbContext: IdentityDbContext
+    {
+
+    }
+}
